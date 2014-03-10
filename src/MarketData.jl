c06000a5999661e6b7f06ccca350a3b30e954830
milktrader <milktrader@gmail.com>
2014-03-10 09:45:48 -0400

    made Adj Cl to Ad Close since readtimearray can handle it now

--- src/MarketData.jl
+++ src/MarketData.jl
@@ -17,21 +17,21 @@ export ohlc, op, hi, lo, cl, vm, aj,
   const lo    = ohlc["Low"] 
   const cl    = ohlc["Close"] 
   const vm    = ohlc["Volume"] 
-  const aj    = ohlc["Adj Cl"] 
+  const aj    = ohlc["Adj Close"] 
   const OHLC  = readtimearray(Pkg.dir("MarketData/data/daily_spx_from_1950_to_2013.csv"))
   const Op    = OHLC["Open"] 
   const Hi    = OHLC["High"] 
   const Lo    = OHLC["Low"] 
   const Cl    = OHLC["Close"] 
   const Vm    = OHLC["Volume"]
-  const Aj    = OHLC["Adj Cl"]
+  const Aj    = OHLC["Adj Close"]
   const ohlcm = readtimearray(Pkg.dir("MarketData/data/monthly_spx_from_1980_to_1989.csv"))
   const opm   = ohlcm["Open"] 
   const him   = ohlcm["High"] 
   const lom   = ohlcm["Low"] 
   const clm   = ohlcm["Close"] 
   const vmm   = ohlcm["Volume"]
-  const ajm   = ohlcm["Adj Cl"]
+  const ajm   = ohlcm["Adj Close"]
   
   # const to test dates
   const firstday    = date(1980,1,3)
