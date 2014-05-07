368e01a3b04d7e4f5c5075cb2d028f34d6b9179b
milktrader <milktrader@gmail.com>
2014-05-07 11:38:43 -0400

    starting quandl-ification

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-Old financial series data for testing and developing packages in the JuliaQuant organization.
+MarketData includes historical financial time series datasets. The package provides the data in a TimeArray, which is the TimeSeries data structure.
+
+````julia
+julia> Pkg.add("MarketData")
+
+julia> using MarketData
+````
+
+Data can easily be converted into other data structures. For example, converting the `AAPL` TimeArray into a DataFrame is as simple as this:
+
+````julia
+julia> Apple = DataFrame(Date = AAPL.timestamp, Close = AAPL["Close"].values);
+
+julia> head(Apple)
+6x2 DataFrame
+|-------|------------|-------|
+| Row # | Date       | Close |
+| 1     | 1980-12-12 | 28.75 |
+| 2     | 1980-12-15 | 27.25 |
+| 3     | 1980-12-16 | 25.25 |
+| 4     | 1980-12-17 | 25.88 |
+| 5     | 1980-12-18 | 26.62 |
+| 6     | 1980-12-19 | 28.25 |
+````
--- src/MarketData.jl
+++ src/MarketData.jl
@@ -4,44 +4,20 @@ module MarketData
 
 using Datetime, TimeSeries
 
-export ohlc, op, hi, lo, cl, vm, aj,  
-       OHLC, Op, Hi, Lo, Cl, Vm, Aj,  
-       ohlcm, opm, him, lom, clm, vmm, ajm,  
-       firstday, secondday, tenthday, lastday, 
-       firstmonth, secondmonth, lastmonth
+export AAPL, 
+       BA, 
+       CAT, 
+       cl, op, ohlc # datasets for testing
 
-  # objects to test values
-  const ohlc  = readtimearray(Pkg.dir("MarketData/data/daily_spx_from_1980_to_1981.csv"))
-  const op    = ohlc["Open"] 
-  const hi    = ohlc["High"] 
-  const lo    = ohlc["Low"] 
-  const cl    = ohlc["Close"] 
-  const vm    = ohlc["Volume"] 
-  const aj    = ohlc["Adj Close"] 
-  const OHLC  = readtimearray(Pkg.dir("MarketData/data/daily_spx_from_1950_to_2013.csv"))
-  const Op    = OHLC["Open"] 
-  const Hi    = OHLC["High"] 
-  const Lo    = OHLC["Low"] 
-  const Cl    = OHLC["Close"] 
-  const Vm    = OHLC["Volume"]
-  const Aj    = OHLC["Adj Close"]
-  const ohlcm = readtimearray(Pkg.dir("MarketData/data/monthly_spx_from_1980_to_1989.csv"))
-  const opm   = ohlcm["Open"] 
-  const him   = ohlcm["High"] 
-  const lom   = ohlcm["Low"] 
-  const clm   = ohlcm["Close"] 
-  const vmm   = ohlcm["Volume"]
-  const ajm   = ohlcm["Adj Close"]
-  
-  # const to test dates
-  const firstday    = date(1980,1,3)
-  const secondday   = date(1980,1,4)
-  const tenthday    = date(1980,1,16)
-  const lastday     = date(1981,12,31)
-  const firstmonth  = date(1980,1,3)
-  const secondmonth = date(1980,2,1)
-  const lastmonth   = date(1989,12,1)
+  # US Equities from A to C (so far) 
+  const AAPL = readtimearray(Pkg.dir("MarketData/data/AAPL.csv"))
+  const BA   = readtimearray(Pkg.dir("MarketData/data/BA.csv"))
+  const CAT  = readtimearray(Pkg.dir("MarketData/data/CAT.csv"))
 
-  # const to test structure
+  # smaller datasets used for testing time-related packages
+  const cl   = AAPL["Close"][date(2000,1,1):date(2001,12,31)]
+  const op   = AAPL["Open"][date(2000,1,1):date(2001,12,31)]
+  const ohlc = AAPL["Open", "High", "Low", "Close"][date(2000,1,1):date(2001,12,31)] 
+
 
 end
