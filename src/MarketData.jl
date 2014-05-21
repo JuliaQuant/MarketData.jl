b79628f831bcbbd3e51f0a2a08422f2f6c9d9235
milktrader <milktrader@gmail.com>
2014-05-21 11:31:26 -0400

    added ohlcv object closes #6

--- src/MarketData.jl
+++ src/MarketData.jl
@@ -7,7 +7,7 @@ using Datetime, TimeSeries
 export AAPL, 
        BA, 
        CAT, 
-       cl, op, ohlc # datasets for testing
+       cl, op, ohlc, ohlcv # datasets for testing
 
   # US Equities from A to C (so far) 
   const AAPL = readtimearray(Pkg.dir("MarketData/data/AAPL.csv"))
@@ -15,9 +15,10 @@ export AAPL,
   const CAT  = readtimearray(Pkg.dir("MarketData/data/CAT.csv"))
 
   # smaller datasets used for testing time-related packages
-  const cl   = AAPL["Close"][date(2000,1,1):date(2001,12,31)]
-  const op   = AAPL["Open"][date(2000,1,1):date(2001,12,31)]
-  const ohlc = AAPL["Open", "High", "Low", "Close"][date(2000,1,1):date(2001,12,31)] 
+  const cl    = AAPL["Close"][date(2000,1,1):date(2001,12,31)]
+  const op    = AAPL["Open"][date(2000,1,1):date(2001,12,31)]
+  const ohlc  = AAPL["Open", "High", "Low", "Close"][date(2000,1,1):date(2001,12,31)] 
+  const ohlcv = AAPL["Open", "High", "Low", "Close", "Volume"][date(2000,1,1):date(2001,12,31)] 
 
 
 end
