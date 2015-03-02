bb03c9ad84d0270f414b57823e7a37d0571b90b1
milktrader <milktrader@gmail.com>
2015-03-02 17:32:58 -0500

    moving conf.py up one level

--- docs/conf.py
+++ docs/conf.py
@@ -14,13 +14,14 @@
 
 import sys, os, re
 
+import juliadoc
+import sphinx_rtd_theme
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 
-import juliadoc
-import sphinx_rtd_theme
 
 # -- General configuration ------------------------------------------------
 
