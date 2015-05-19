85e216a9e55d4d2b882ff7b5cd3e1aeae79da5b7
milktrader <milktrader@gmail.com>
2015-05-19 09:16:34 -0400

    changed seconds_data to datetime1 and added datetime2

--- src/MarketData.jl
+++ src/MarketData.jl
@@ -16,7 +16,8 @@ export AAPL,
        BA,
        CAT,
        cl, op, ohlc, ohlcv,
-       seconds_data, mdata
+       datetime1, datetime2,
+       mdata
 
   # US Equities from A to C (so far) 
   const AAPL = readtimearray(Pkg.dir("MarketData/data/AAPL.csv"))
@@ -24,11 +25,12 @@ export AAPL,
   const CAT  = readtimearray(Pkg.dir("MarketData/data/CAT.csv"))
 
   # smaller datasets used for testing time-related packages
-  const cl           = AAPL["Close"][Date(2000,1,1):Date(2001,12,31)]
-  const op           = AAPL["Open"][Date(2000,1,1):Date(2001,12,31)]
-  const ohlc         = AAPL["Open", "High", "Low", "Close"][Date(2000,1,1):Date(2001,12,31)] 
-  const ohlcv        = AAPL["Open", "High", "Low", "Close", "Volume"][Date(2000,1,1):Date(2001,12,31)] 
-  const seconds_data = readtimearray(Pkg.dir("MarketData/data/seconds_data.csv"))["Close"] # seconds data
-  const mdata        = TimeArray(cl.timestamp, cl.values, cl.colnames, "Apple")     # data with meta field occupied
+  const cl        = AAPL["Close"][Date(2000,1,1):Date(2001,12,31)]
+  const op        = AAPL["Open"][Date(2000,1,1):Date(2001,12,31)]
+  const ohlc      = AAPL["Open", "High", "Low", "Close"][Date(2000,1,1):Date(2001,12,31)] 
+  const ohlcv     = AAPL["Open", "High", "Low", "Close", "Volume"][Date(2000,1,1):Date(2001,12,31)] 
+  const datetime1 = readtimearray(Pkg.dir("MarketData/data/datetime1.csv"))
+  const datetime2 = readtimearray(Pkg.dir("MarketData/data/datetime2.csv"))
+  const mdata     = TimeArray(cl.timestamp, cl.values, cl.colnames, "Apple") # data with meta field occupied
 
 end
