1c80f53296b81dea5e38e2e843c4df3c4d7114df
milktrader <milktrader@gmail.com>
2015-09-15 21:14:40 -0400

    converting package to support Julia 0.4 only

--- test/readwrite.jl
+++ test/readwrite.jl
@@ -3,37 +3,37 @@ using MarketData
 facts("TimeSeries readwrite parses csv file correctly") do
 
     context("1d values array works") do
-        @fact typeof(cl.values) => Array{Float64,1}
+        @fact typeof(cl.values) --> Array{Float64,1}
     end
   
     context("2d values array works") do
-        @fact typeof(ohlc.values) => Array{Float64,2}
+        @fact typeof(ohlc.values) --> Array{Float64,2}
     end
   
     context("timestamp parses to correct type") do
-        @fact typeof(cl.timestamp)        => Vector{Date}
-        @fact typeof(datetime1.timestamp) => Vector{DateTime}
+        @fact typeof(cl.timestamp)        --> Vector{Date}
+        @fact typeof(datetime1.timestamp) --> Vector{DateTime}
     end
 
     context("meta field is correctly constructed") do
-        @fact mdata.meta => "Apple"
+        @fact mdata.meta --> "Apple"
     end
 end
 
 facts("const objects have expected length") do
 
     context("test objects") do
-        @fact length(datetime1) => 5
-        @fact length(mdata)     => 500
-        @fact length(op)        => 500
-        @fact length(cl)        => 500
-        @fact length(ohlc)      => 500
-        @fact length(ohlcv)     => 500
+        @fact length(datetime1) --> 5
+        @fact length(mdata)     --> 500
+        @fact length(op)        --> 500
+        @fact length(cl)        --> 500
+        @fact length(ohlc)      --> 500
+        @fact length(ohlcv)     --> 500
     end
 
     context("historical data sets") do
-        @fact length(AAPL) => 8336
-        @fact length(BA)   => 13090
-        @fact length(CAT)  => 13090
+        @fact length(AAPL) --> 8336
+        @fact length(BA)   --> 13090
+        @fact length(CAT)  --> 13090
     end
 end
