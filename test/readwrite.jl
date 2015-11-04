f66f78ddc5da5abbabaabb79968437f331cc3eec
milktrader <milktrader@gmail.com>
2015-11-03 22:02:06 -0500

    removed test of mdata object

--- test/readwrite.jl
+++ test/readwrite.jl
@@ -28,7 +28,6 @@ facts("const objects have expected length") do
 
     context("test objects") do
         @fact length(datetime1) --> 5
-        @fact length(mdata)     --> 500
         @fact length(op)        --> 500
         @fact length(cl)        --> 500
         @fact length(ohlc)      --> 500
