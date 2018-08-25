b8a78e89e2acb3dc1b7297878c932d0c8fedc1dc
Iblis Lin <iblis@hs.ntnu.edu.tw>
2018-08-25 22:11:53 +0800

    fix depwarn on date range object

--- src/const.jl
+++ src/const.jl
@@ -12,10 +12,12 @@
   const TX   = readtimearray(joinpath(DATADIR, "TX.csv"),   meta = "TX")
 
   # smaller datasets used for testing time-related packages
-  const cl        = AAPL["Close"][Date(2000,1,1):Date(2001,12,31)]
-  const op        = AAPL["Open"][Date(2000,1,1):Date(2001,12,31)]
-  const ohlc      = AAPL["Open", "High", "Low", "Close"][Date(2000,1,1):Date(2001,12,31)]
-  const ohlcv     = AAPL["Open", "High", "Low", "Close", "Volume"][Date(2000,1,1):Date(2001,12,31)]
+  const dates = Date(2000,1,1):Day(1):Date(2001,12,31)
+  const cl        = AAPL["Close"][dates]
+  const op        = AAPL["Open"][dates]
+  const ohlc      = AAPL["Open", "High", "Low", "Close"][dates]
+  const ohlcv     = AAPL["Open", "High", "Low", "Close", "Volume"][dates]
   const datetime1 = readtimearray(joinpath(DATADIR, "datetime1.csv"))
-  const datetime2 = readtimearray(joinpath(DATADIR, "datetime2.csv"), format="yyyy-mm-dd HH:MM:SS")
+  const datetime2 = readtimearray(joinpath(DATADIR, "datetime2.csv"),
+                                  format="yyyy-mm-dd HH:MM:SS")
   const mdata     = TimeArray(cl.timestamp, cl.values, cl.colnames, "Apple") # data with meta field occupied
