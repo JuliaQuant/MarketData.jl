876f8df572f477b2378ab67449baa473123e3996
Iblis Lin <iblis@hs.ntnu.edu.tw>
2018-10-11 11:09:25 +0800

    Deprecate the helper functions (#51)
    * Deprecate the helper functions
    
    Since we are moving toward the symbol indexing.
    Ref JuliaStats/TimeSeries.jl#377
    
    * Update NEWS.md
    
    * travis: add 1.0 to build matrix
    
    - update doc deploy script

--- src/helpers.jl
+++ src/helpers.jl
@@ -1,7 +1,3 @@
 for (fn, name) in [(:o, "Open"), (:h, "High"), (:l, "Low"), (:c, "Close"), (:v, "Volume")]
-    @eval begin
-        function $fn(ta::TimeArray)
-            ta[$name]
-        end
-    end
+    @eval @deprecate $fn(ta::TimeArray) getindex(ta, $name)
 end
