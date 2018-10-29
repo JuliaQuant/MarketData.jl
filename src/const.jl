8e4afeab47f88d017ac5feb27b2cff3e2add9154
Iblis Lin <iblis@hs.ntnu.edu.tw>
2018-10-29 20:50:20 +0800

    TimeSeries.jl symbol indexing (#54)
    Closes #55

--- src/const.jl
+++ src/const.jl
@@ -13,10 +13,10 @@
 
   # smaller datasets used for testing time-related packages
   const dates = Date(2000,1,1):Day(1):Date(2001,12,31)
-  const cl        = AAPL["Close"][dates]
-  const op        = AAPL["Open"][dates]
-  const ohlc      = AAPL["Open", "High", "Low", "Close"][dates]
-  const ohlcv     = AAPL["Open", "High", "Low", "Close", "Volume"][dates]
+  const cl        = AAPL[:Close][dates]
+  const op        = AAPL[:Open][dates]
+  const ohlc      = AAPL[:Open, :High, :Low, :Close][dates]
+  const ohlcv     = AAPL[:Open, :High, :Low, :Close, :Volume][dates]
   const datetime1 = readtimearray(joinpath(DATADIR, "datetime1.csv"))
   const datetime2 = readtimearray(joinpath(DATADIR, "datetime2.csv"),
                                   format="yyyy-mm-dd HH:MM:SS")
--- src/helpers.jl
+++ src/helpers.jl
@@ -1,3 +1,3 @@
-for (fn, name) in [(:o, "Open"), (:h, "High"), (:l, "Low"), (:c, "Close"), (:v, "Volume")]
+for (fn, name) in [(:o, :Open), (:h, :High), (:l, :Low), (:c, :Close), (:v, :Volume)]
     @eval @deprecate $fn(ta::TimeArray) getindex(ta, $name)
 end
