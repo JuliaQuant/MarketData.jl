88501f77584a08a2c9f4adfd0cdad159e81d0ebf
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-08-09 07:30:17 +0000

    build based on d628574

--- versions.js
+++ versions.js
@@ -1,13 +1,7 @@
 var DOC_VERSIONS = [
   "stable",
-  "latest",
-  "release-0.11",
-  "release-0.10",
-  "release-0.8",
-  "release-0.7",
-  "v0.11.0",
-  "v0.10.1",
-  "v0.8.0",
-  "v0.7.1",
-  "v0.7.0",
+  "v0.11",
+  "v0.10",
+  "v0.8",
+  "v0.7",
 ];
