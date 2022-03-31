ec68f6c5cf2f61d41cf092b46991fba92dd3da2a
Danny Winrow <dwinrow@boal.co.uk>
2022-03-31 12:24:46 +0100

    updated missingstring for fred - DGS fixed

--- src/downloads.jl
+++ src/downloads.jl
@@ -134,7 +134,7 @@ function fred(data::String="CPIAUCNS")
     url = "http://research.stlouisfed.org/fred2/series/$data/downloaddata/$data.csv"
     res = HTTP.get(url)
     @assert res.status == 200
-    csv = CSV.File(res.body)
+    csv = CSV.File(res.body, missingstring=".")
     sch = TimeSeries.Tables.schema(csv)
     TimeArray(csv, timestamp = first(sch.names)) |> cleanup_colname!
 end
