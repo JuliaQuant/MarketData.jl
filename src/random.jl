b29b497ca256357695b7e409c92793cb1a50a7b6
FemtoTrader <femto.trader@gmail.com>
2024-01-05 11:27:25 +0100

    Create random.jl

---
+++ src/random.jl
@@ -0,0 +1,122 @@
+using Dates
+
+function random_cl(;
+    start = Dates.DateTime(2020, 1, 1),
+    step = Dates.Minute(1),
+    length = 500,
+    price_init = nothing,
+    price_init_min = 1.00,
+    price_init_step = 0.01,
+    price_init_max = 1000.00,
+    price_var_min = -1.0,
+    price_var_step = 0.01,
+    price_var_max = 0.01,
+)
+    idx = range(start, length = length, step = step)
+    if isnothing(price_init)
+        price_init = rand(price_init_min:price_init_step:price_init_max)
+    end
+    return TimeArray(
+        collect(idx),
+        price_init .+ cumsum(rand(price_var_min:price_var_step:price_var_max, length)),
+        [:Close],
+    )
+end
+
+function random_vol(;
+    start = Dates.DateTime(2020, 1, 1),
+    step = Dates.Minute(1),
+    length = 500,
+    vol_init_min = 0.0,
+    vol_init_step = 0.1,
+    vol_init_max = 100.0,
+)
+    idx = range(start, length = length, step = step)
+    return TimeArray(
+        collect(idx),
+        rand(vol_init_min:vol_init_step:vol_init_max, length),
+        [:Volume],
+    )
+end
+
+function random_ohlc(;
+    start = Dates.DateTime(2020, 1, 1),
+    step = Dates.Hour(1),
+    sub_step = Dates.Minute(1),
+    length = 500,
+    price_init = nothing,
+    price_init_min = 1.00,
+    price_init_step = 0.01,
+    price_init_max = 1000.00,
+    price_var_min = -1.0,
+    price_var_step = 0.01,
+    price_var_max = 0.01,
+    vol_init_min = 0.0,
+    vol_init_step = 0.1,
+    vol_init_max = 100.0,
+)
+    idx = range(start, step = step, length = length)
+    nsub = ceil(Int, step / sub_step)
+    price = random_cl(
+        start = start,
+        step = sub_step,
+        length = length * nsub,
+        price_init = price_init,
+        price_init_min = price_init_min,
+        price_init_step = price_init_step,
+        price_init_max = price_init_max,
+        price_var_min = price_var_step,
+        price_var_max = price_var_max,
+    )
+    price
+    ta_o = collapse(price, step, first, first)
+    ta_h = collapse(price, step, first, maximum)
+    ta_l = collapse(price, step, first, minimum)
+    ta_c = collapse(price, step, first, last)
+    a_ohlc = hcat(values(ta_o), values(ta_h), values(ta_l), values(ta_c))
+    return TimeArray(timestamp(ta_o), a_ohlc, [:Open, :High, :Low, :Close])
+end
+
+
+function random_ohlcv(;
+    start = Dates.DateTime(2020, 1, 1),
+    step = Dates.Hour(1),
+    sub_step = Dates.Minute(1),
+    length = 500,
+    price_init = nothing,
+    price_init_min = 1.00,
+    price_init_step = 0.01,
+    price_init_max = 1000.00,
+    price_var_min = -1.0,
+    price_var_step = 0.01,
+    price_var_max = 0.01,
+    vol_init_min = 0.0,
+    vol_init_step = 0.1,
+    vol_init_max = 100.0,
+)
+    ta_ohlc = random_ohlc(;
+        start = start,
+        step = step,
+        sub_step = sub_step,
+        length = length,
+        price_init = price_init,
+        price_init_min = price_init_min,
+        price_init_step = price_init_step,
+        price_init_max = price_init_max,
+        price_var_min = price_var_min,
+        price_var_step = price_var_step,
+        price_var_max = price_var_max,
+        vol_init_min = vol_init_min,
+        vol_init_step = vol_init_step,
+        vol_init_max = vol_init_max,
+    )
+    ta_vol = random_vol(;
+        start = start,
+        step = step,
+        length = Base.length(ta_ohlc),
+        vol_init_min = vol_init_min,
+        vol_init_step = vol_init_step,
+        vol_init_max = vol_init_max,
+    )
+    hcat(ta_ohlc, ta_vol)
+end
